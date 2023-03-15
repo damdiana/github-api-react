81ef37996673099d14afb944648a262f1baa0d0a
Diana <117006466+damdiana@users.noreply.github.com>
2023-03-15 15:32:52 +0200

    [COSMETIC] Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Button from './components/Button/Button'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -32,7 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
