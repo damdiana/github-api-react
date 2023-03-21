7bc9ef64f9afe2bdad8948c0b226805325d138f7
Diana <117006466+damdiana@users.noreply.github.com>
2023-03-21 12:15:35 +0200

    Create <InfoCard> Component (#9)

---
+++ src/components/InfoCard/InfoCard.tsx
@@ -0,0 +1,31 @@
+import React, { PropsWithChildren } from 'react';
+import './InfoCard.css';
+
+type Props = {
+  title: {
+    content: string;
+    href: string;
+  };
+  pairs: Record<string, string>;
+};
+
+const InfoCard = ({ title, pairs }: Props) => {
+  return (
+    <div className="dialog-background">
+      <a href={title.href} target="_blank" className="title-hover">
+        <span className="text-bold">👉🏻 Title: </span>
+        <span> {title.content} </span>
+      </a>
+      {Object.entries(pairs).map(([category, value]) => {
+        return (
+          <p key={category}>
+            <span className="text-bold"> {category}: </span>
+            {value}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
+
+export default InfoCard;
