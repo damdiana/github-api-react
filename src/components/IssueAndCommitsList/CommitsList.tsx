f94649a5f8ed4f1c21c3a221296be1e1571c8c3d
Diana <117006466+damdiana@users.noreply.github.com>
2023-04-13 16:44:01 +0300

    Implement pagination for issues and commits #32

--- src/components/IssueAndCommitsList/CommitsList.tsx
+++ src/components/IssueAndCommitsList/CommitsList.tsx
@@ -1,20 +1,34 @@
 import { useEffect, useState } from 'react';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import {
   GithubCommits,
   GithubIssue,
   GithubRepo,
   fetchGithubCommits,
   fetchGithubIssues,
-} from '../GithubAPI';
-import InfoCard from './InfoCard/InfoCard';
-import { formatDateTime } from '../services/utils';
-import LoadingSpinner from './LoadingSpinner/LoadingSpinner';
+} from '../../GithubAPI';
+import InfoCard from '../InfoCard/InfoCard';
+import { formatDateTime } from '../../services/utils';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
+import Button from '../Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const CommitsList = ({ repo }: { repo: GithubRepo }) => {
   const [commits, setCommits] = useState<GithubCommits[]>([]);
   const [erroMessage, setErrorMessage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [errorType, setErrorType] = useState<'fatal' | 'nofatal'>('fatal');
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [hasNextPage, setHasNextPage] = useState(false);
+  const [hasPreviousPage, setHasPreviousPage] = useState(false);
+
+  const showNextPage = () => {
+    setPageNumber(pageNumber + 1);
+  };
+
+  const showPreviousPage = () => {
+    setPageNumber(pageNumber - 1);
+  };
 
   const fetchAndDisplayCommits = async () => {
     setIsLoading(true);
@@ -22,12 +36,14 @@ const CommitsList = ({ repo }: { repo: GithubRepo }) => {
       const resp = await fetchGithubCommits(
         repo.owner.login,
         repo.name,
-        100,
-        1
+        10,
+        pageNumber
       );
       if (resp.ok) {
         setCommits(resp.commits);
         setErrorMessage('');
+        setHasNextPage(resp.hasNextPage);
+        setHasPreviousPage(resp.hasPreviousPage);
       } else {
         setCommits([]);
         setErrorType('nofatal');
@@ -43,7 +59,7 @@ const CommitsList = ({ repo }: { repo: GithubRepo }) => {
 
   useEffect(() => {
     fetchAndDisplayCommits();
-  }, [repo.name]);
+  }, [repo.name, pageNumber]);
 
   return (
     <div>
@@ -67,6 +83,21 @@ const CommitsList = ({ repo }: { repo: GithubRepo }) => {
           }}
         />
       ))}
+      {erroMessage === '' && (
+        <div className="footer text-center">
+          {hasPreviousPage === true && (
+            <Button variant="text" color="navy" onClick={showPreviousPage}>
+              <FontAwesomeIcon icon={faAngleLeft} /> Prev
+            </Button>
+          )}
+          <span className="page-number"> {pageNumber} </span>
+          {hasNextPage === true && (
+            <Button variant="text" color="navy" onClick={showNextPage}>
+              Next <FontAwesomeIcon icon={faAngleRight} />
+            </Button>
+          )}
+        </div>
+      )}
     </div>
   );
 };
